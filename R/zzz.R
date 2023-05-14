155c4bc214e5b6bc06e9cbc747fe9b58dddd9539
Xin Gao <132988390+xingao0612@users.noreply.github.com>
2023-05-14 20:23:23 +0800

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to AgingPred!")
+packageStartupMessage("Please use the command \'AgingPred()\' to start using our package.")
+}
